8c40d556ff507c32f9a65551cc9e97637b9df75f
Sergei Tolshin <tolshin.sergei@yandex.ru>
2026-09-14 22:24:41 +0500

    Шаг 3

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import {processFormData} from "./lib/utils.js";
 import {initTable} from "./components/table.js";
 // @todo: подключение
 import {initPagination} from "./components/pagination.js";
+import {initSorting} from "./components/sorting.js";
 
 
 // Исходные данные используемые в render()
@@ -39,6 +40,7 @@ function render(action) {
     let result = [...data]; // копируем для последующего изменения
     // @todo: использование
     result = applyPagination(result, state, action);
+    result = applySorting(result, state, action);
 
 
     sampleTable.render(result)
@@ -47,7 +49,7 @@ function render(action) {
 const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
-    before: [],
+    before: ['header'],
     after: ['pagination']
 }, render);
 
@@ -63,6 +65,11 @@ const applyPagination = initPagination(
         return el;
     }
 );
+
+const applySorting = initSorting([
+    sampleTable.header.elements.sortByDate,
+    sampleTable.header.elements.sortByTotal
+]);
 
 
 const appRoot = document.querySelector('#app');
