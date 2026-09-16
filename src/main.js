3b1e52b067f3c7ab4c6f9000a64a8350d4b70b69
Sergei Tolshin <tolshin.sergei@yandex.ru>
2026-09-16 21:51:01 +0500

    Шаг 5

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import {initTable} from "./components/table.js";
 import {initPagination} from "./components/pagination.js";
 import {initSorting} from "./components/sorting.js";
 import {initFiltering} from "./components/filtering.js";
+import {initSearching} from "./components/searching.js";
 
 
 // Исходные данные используемые в render()
@@ -40,9 +41,10 @@ function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
     // @todo: использование
-    result = applyPagination(result, state, action);
-    result = applySorting(result, state, action);
+    result = applySearching(result, state, action);
     result = applyFiltering(result, state, action);
+    result = applySorting(result, state, action);
+    result = applyPagination(result, state, action);
 
 
     sampleTable.render(result)
@@ -51,7 +53,7 @@ function render(action) {
 const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
-    before: ['header', 'filter'],
+    before: ['search', 'header', 'filter'],
     after: ['pagination']
 }, render);
 
@@ -76,6 +78,8 @@ const applySorting = initSorting([
 const applyFiltering = initFiltering(sampleTable.filter.elements, {
     searchBySeller: indexes.sellers
 });
+
+const applySearching = initSearching('search');
 
 
 const appRoot = document.querySelector('#app');
